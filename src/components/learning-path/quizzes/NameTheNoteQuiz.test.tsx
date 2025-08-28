14b29e1e1d71495eb3bd629ad429ed2d9f36990a
SquidgyG <david-gill@live.com>
2025-08-27 20:12:49 -0700

    test: import expect and test from vitest

--- src/components/learning-path/quizzes/NameTheNoteQuiz.test.tsx
+++ src/components/learning-path/quizzes/NameTheNoteQuiz.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import NameTheNoteQuiz from './NameTheNoteQuiz';
-import { vi } from 'vitest';
+import { expect, test, vi } from 'vitest';
 
 const initAudioMock = vi.fn();
 const playNoteMock = vi.fn();
